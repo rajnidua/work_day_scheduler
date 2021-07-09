d63fad5b9d5cfbf4ae4aba61d84710d07738549f
Rajni <rajni.dua14@gmail.com>
2021-07-09 23:21:49 +0930

    fixed the background color issue, cleaned the code and added comments and README

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # work_day_scheduler
+
 Save your tasks by hour, all past, present and future tasks are displayed in different colors
+
+Your are presented with a work day scheduler with the "current day" displayed at the top.
+
+You are presented with a work day scheduler that displays hourly time blocks in the form of rows.
+
+rows are displayed in three different colors for past,present and future time blocks relative to the current hour.
+
+user can enter tasks in the time blocks.
+
+when user presses save button, the data is saved in the local storage as an object with id and text as its elements
+
+when user refreshes the page, the data persists.
+
+user can change the data by deleting the current text and entering a new text.
+
+user then saves the altered data by a double click on save button.
+
+single click on altered data removes it from local storage and the next click add the changed data in the local storage.
+
+live app link : https://rajnidua.github.io/work_day_scheduler/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,92 +1,120 @@
-var myHour = 1;
+var myHour = 9;
 var morningOrEvening = ":00am";
-var timeAfter12 = 1;
-var timeBefore12 =1;
 var myTime;
 var myStoredRecord = [];
 var myStoredRecordName = 'my_StoredRecord';
 
-$('#currentDay').text(moment());
+//display the current day
+$('#currentDay').text(moment().format("MMM Do, YYYY"));
 
-function showDayScheduler(){
-for(rowNum=0;rowNum<24;rowNum++){
 
+//display the calender on the page
+function showDayScheduler()
+{
+    for(rowNum=0;rowNum<12;rowNum++)
+    {
+        myTime = myHour + morningOrEvening;
+        $myRowBlock = $("<tr>").addClass("row time-block");
+        $myTimeBlock = $("<td>").addClass(" col col-md-2 hour").text(myTime);
+        $myTextBlock = $("<textarea>").addClass("col col-md-8 textarea").text("").attr("id",myTime);
+        $myButtonBlock = $("<button>").addClass("col col-md-2 save-btn").text("save");
     
-    myTime = myHour + morningOrEvening;
+        $myRowBlock.append($myTimeBlock,$myTextBlock,$myButtonBlock);
+        $("#myTable").append($myRowBlock);
+        //display the background color from past, present and future classes
+        getTheBackgroundColour(); 
+        //get the time blocks for displaying the first column of the table
+        getTheTime();
+    }
+}
+
+function getTheBackgroundColour()
+{
     var now = moment();
-    var currentHour = now.hour();
-    //var currentHour = 1;
+    var currentHour = moment(myDate).format('h');
     var systemMorningOrEvening = moment(myDate).format('a');
-    //var systemMorningOrEvening = 'am';
-console.log(currentHour);
+   
+    console.log("The current hour is "+currentHour);
 
-
-    $myRowBlock = $("<tr>").addClass("row time-block");
-    $myTimeBlock = $("<td>").addClass(" col col-md-2 hour").text(myTime);
-    $myTextBlock = $("<textarea>").addClass("col col-md-8 textarea").text("").attr("id",myTime);
-    $myButtonBlock = $("<button>").addClass("col col-md-2 save-btn").text("save");
     var myDate = new Date();
-    console.log("console in "+moment(myDate).format('h a'));
-    console.log("fff"+morningOrEvening.substr(3,5)+"jjjj"+systemMorningOrEvening);
-    if(morningOrEvening.substr(3,5) === 'am' && systemMorningOrEvening ==='am'){
-        console.log("one "+morningOrEvening + systemMorningOrEvening);
-        if(myHour<currentHour){
+    
+    if(morningOrEvening.substr(3,5) === 'am' && systemMorningOrEvening ==='am')
+    {
+        console.log("my hour is " + myHour);
+        console.log("current hour is " + currentHour);
+        if(myHour<currentHour)
+        {
             $myRowBlock.addClass("past");
-        }else if(myHour===currentHour){
+        }
+        
+        if(myHour===currentHour)
+        {
             $myRowBlock.addClass("present");
         }
-        else{
+        
+        if(myHour>currentHour)
+        {
             $myRowBlock.addClass("future");
         }
     } 
-    if(morningOrEvening.substr(3,5) === 'pm' && systemMorningOrEvening ==='pm'){
-        console.log("two "+morningOrEvening + systemMorningOrEvening);
-        if(myHour<currentHour){
-            $myRowBlock.addClass("past");
-        }else if(myHour===currentHour){
+    if(morningOrEvening.substr(3,5) === 'pm' && systemMorningOrEvening ==='pm')
+    {
+        console.log("pm block");
+        console.log("my hour is " + myHour);
+        console.log("current hour is " + currentHour);
+        if(myHour == currentHour)
+        {
+            console.log("i am in present block");
             $myRowBlock.addClass("present");
         }
-        else{
+        
+        if(myHour === 12 && myHour !== currentHour)
+        {
+            console.log("i am inside first if");
+            $myRowBlock.addClass("past");
+        }
+        
+        if(myHour<currentHour && myHour!==12)
+        {
+            console.log("i am inside second if");
+            $myRowBlock.addClass("past");
+        }
+        
+        if(myHour>currentHour && myHour!==12)
+        {
+            console.log("i am inside forth if");
             $myRowBlock.addClass("future");
         }
     } 
-    if(morningOrEvening.substr(3,5) === 'pm' && systemMorningOrEvening ==='am'){
-        console.log("three "+morningOrEvening + systemMorningOrEvening);
+    if(morningOrEvening.substr(3,5) === 'pm' && systemMorningOrEvening ==='am')
+    {
+        console.log("pm and am block");
+        console.log("my hour is " + myHour);
+        console.log("current hour is " + currentHour);
         $myRowBlock.addClass("future");
         $myTextBlock.addClass("future");
     }
 
-    if(morningOrEvening.substr(3,5) === 'am' && systemMorningOrEvening ==='pm'){
-        console.log("four "+morningOrEvening + systemMorningOrEvening);
+    if(morningOrEvening.substr(3,5) === 'am' && systemMorningOrEvening ==='pm')
+    {
+        console.log("pm and am block");
+        console.log("my hour is " + myHour);
+        console.log("current hour is " + currentHour);
         $myRowBlock.addClass("past");
     }
     
-    
-    $myRowBlock.append($myTimeBlock,$myTextBlock,$myButtonBlock);
-    
-$("#myTable").append($myRowBlock);
-getTheTime();
-console.log("time created");
-console.log(moment().toDate());
-var now = moment();
-var currentHour = now.hour();
-console.log(currentHour);
-var time = now.hour() + ':00';
-var testTime ="2:00am";
-time = time + ((now.hour()) >= 12 ? 'pm' : ' am');
-console.log("the time is"+time);
-
- }
 }
 
 showDayScheduler();
 GetStoredBlocks();
 
-function getTheTime(){
-if (myHour===12){
+function getTheTime()
+{
+    if (myHour===12)
+    {      
     myHour=1;
-}
-else if(myHour === 11)
+    }
+    else if(myHour === 11)
     {
         morningOrEvening = ":00pm";
         myHour++;
@@ -97,21 +125,22 @@ else if(myHour === 11)
      
 } 
 
-//console.log("i am here"+ $(".save-btn")) ;
-$(".save-btn").click(saveData);
 
-function saveData(){
+$(".save-btn").click(saveData);
+//this functions calls when the save button is clicked
+function saveData()
+{
     console.log("Iam inside data");
     $correspondingTextArea = $($(this).parent().children()[1]);
     textContent = $correspondingTextArea.val();
     textId = $correspondingTextArea.attr("id");
     console.log("value inside the text area is "+textContent+ "id value is "+textId);
-
     addData(textContent,textId);
-
 }
 
-function addData(myText,myId){
+//add a new record in the local storage
+function addData(myText,myId)
+{
     console.log("i am in add data");
     myRecord = {
         myRecordText : myText,
@@ -119,79 +148,31 @@ function addData(myText,myId){
     };
 
     console.log("The value of my record text is "+myRecord.myRecordText +"and the value of my record id is"+myRecord.myRecordId);
-     for(var i = 0; i < myStoredRecord.length; i++)
+    for(var i = 0; i < myStoredRecord.length; i++)
     {
         if(myStoredRecord[i].myRecordId === myRecord.myRecordId)
         {
             myStoredRecord.splice(i, 1);
-
             localStorage.setItem(myStoredRecordName, JSON.stringify(myStoredRecord));
-
             return null;
         }
     }
-
-
     myStoredRecord.push(myRecord);
     localStorage.setItem(myStoredRecordName,JSON.stringify(myStoredRecord));
 }
 
-
+//extracting data from local storage and populating it to the text area for their corresponding ids
 function GetStoredBlocks()
 {
-   console.log ("i am in getstoredblocks");
+    console.log ("i am in getstoredblocks");
     if(localStorage.getItem(myStoredRecordName))
     {
         myStoredRecord = JSON.parse(localStorage.getItem(myStoredRecordName));
-
         myStoredRecord.forEach(myRowBlock => {
-           
             textId = "#" + myRowBlock.myRecordId;
-
-             $(document.getElementById(myRowBlock.myRecordId)).val(myRowBlock.myRecordText);
-
-           // $myTextBlock.val(myRowBlock.myRecordText);
-
-          //  $myTextBlock = $(($myRowBlock).children()[1])
-            
-//            $iLock.toggleClass("unlocked");
-
+            $(document.getElementById(myRowBlock.myRecordId)).val(myRowBlock.myRecordText);
         });
-
     }
-
 }
 
-
-/* function renderSavedData(){
-    if(localStorage.getItem(myStoredRecordName)){
-        myStoredRecord = JSON.parse(localStorage.getItem(myStoredRecordName))
-        myStoredRecord.forEach(myRowBlock => {
-            textId =  "#"+myRowBlock.myRecordId;
-                console.log("so my new id is "+textId);
-                textContent = myRowBlock.myRecordText;
-                console.log(textContent);
-               $(( ($myRowBlock).children()[1])).attr("id",myRowBlock.myRecordId).val =textContent;
-               console.log(($myRowBlock).children()[1]); */
-                //$myTextBlock.attr("id",textId).text(textContent);
-//$myTextBlock = $(document.getElementById(textId)).text(myTextBlock.innerHTML);
-//console.log($myTextBlock);
-           /*  console.log("My latest row is"+$myRowBlock);
-           // $myTextBlock.val(myRecordText);
-           $myTextArea.val(myRowBlock.myRecordText);
-            console.log("My text value should be"+myRowBlock.myRecordText);
-           // console.log("text inside is "+$myTextArea.val());
-            textContent = myRowBlock.myRecordText;
-            //$iLock = $(($myRowBlock).parent().children().children()[1])
-           $($(myRowBlock).children()[1]).val =textContent;
-            
-            console.log($myRowBlock.attr("background-color:red;"));
-            //$iLock.toggleClass("unlocked"); */
-
-            
-  /*       });
-    }
-} */
-
- 
 
